9a49703ba0297cdaab873f03599d17b591f1ee0e
Manuel Jenny <git@manueljenny.ch>
2021-03-25 07:58:21 +0100

    fix(exception): consolidate exception handling

--- java-core/src/main/java/com/pcloud/sdk/ApiClient.java
+++ java-core/src/main/java/com/pcloud/sdk/ApiClient.java
@@ -84,6 +84,7 @@ public interface ApiClient {
      *
      * @param path {@link RemoteFolder} path
      * @return {@link Call}
+     * @throws IllegalArgumentException on a null {@code path} argument.
      */
     Call<RemoteFolder> listFolder(String path);
 
@@ -97,6 +98,7 @@ public interface ApiClient {
      * @param path    target folder path
      * @param recursively if true, a full folder tree will be returned, otherwise the resulting {@linkplain RemoteFolder folder} will contain only its direct children
      * @return {@link Call} resulting in a {@link RemoteFolder} instance holding the metadata for the requested fodler id.
+     * @throws IllegalArgumentException on a null {@code path} argument.
      */
     Call<RemoteFolder> listFolder(String path, boolean recursively);
 
@@ -247,16 +249,6 @@ public interface ApiClient {
 
     /**
      * Change a specified folder's parent.
-     * <p>For more information, see the related <a href="https://docs.pcloud.com/methods/folder/renamefolder.html" target="_blank">documentation page</a>.</p>
-     *
-     * @param path   The path of the folder you would like to move
-     * @param toPath The path of the new parent folder
-     * @return {@link Call} resulting in the moved folder's metadata.
-     */
-    Call<RemoteFolder> moveFolder(String path, String toPath);
-
-    /**
-     * Change a specified folder's parent.
      *
      * @param folder   The {@link RemoteFolder} you would like to move. Must not be null.
      * @param toFolder The new parent {@link RemoteFolder}. Must not be null.
@@ -266,6 +258,18 @@ public interface ApiClient {
      * @see #moveFolder(long, long) #moveFolder(long, long)
      */
     Call<RemoteFolder> moveFolder(RemoteFolder folder, RemoteFolder toFolder);
+
+    /**
+     * Change a specified folder's parent.
+     * <p>For more information, see the related <a href="https://docs.pcloud.com/methods/folder/renamefolder.html" target="_blank">documentation page</a>.</p>
+     *
+     * @param path   The path of the folder you would like to move
+     * @param toPath The path of the new parent folder
+     * @return {@link Call} resulting in the moved folder's metadata.
+     * @throws IllegalArgumentException on a null {@code path} argument.
+     * @throws IllegalArgumentException on a null {@code toPath} argument.
+     */
+    Call<RemoteFolder> moveFolder(String path, String toPath);
 
     /**
      * Copy specified folder.
@@ -1039,6 +1043,7 @@ public interface ApiClient {
      *
      * @param path target file path.
      * @return {@link Call} resulting in a {@link RemoteFile} instance holding the metadata for the requested path.
+     * @throws IllegalArgumentException on a null {@code path} argument.
      */
     Call<RemoteFile> loadFile(String path);
 
@@ -1063,6 +1068,7 @@ public interface ApiClient {
      *
      * @param path target folder path.
      * @return {@link Call} resulting in a {@link RemoteFolder} instance holding the metadata for the requested path.
+     * @throws IllegalArgumentException on a null {@code path} argument.
      */
     Call<RemoteFolder> loadFolder(String path);
 
@@ -1105,6 +1111,8 @@ public interface ApiClient {
      * @param path     The path of the file to be moved.
      * @param toPath   The path of the folder where the file will be moved.
      * @return {@link Call} resulting in the metadata of the moved file
+     * @throws IllegalArgumentException on a null {@code path} argument.
+     * @throws IllegalArgumentException on a null {@code toPath} argument.
      */
     Call<RemoteFile> moveFile(String path, String toPath);
 
